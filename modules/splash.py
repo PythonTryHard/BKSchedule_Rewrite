1f3f5920a30e149cf12daa5f807078019ca6135b
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2019-11-19 00:16:53 +0700

    Migrate old splash to here for cleanliness

---
+++ modules/splash.py
@@ -0,0 +1,23 @@
+class Splash():
+    '''
+    Splash screen for the application, tucked away for cleanliness
+    '''
+    def splash(self):
+        print('''
+0------------------------0
+|                        |
+|   BKSchedule Rewrite   |
+|   Copyright (C) 2019   |
+|      GNU GPL v3.0      |
+|                        |
+|  Coder: PythonTryHard  |
+|  Lang : Python 3       |
+|  Ver  : v1.0           |
+|                        |
+|  Phần mềm open-source  |
+|                        |
+| Vì sự tiện lợi của bạn |
+|  không phải là thứ để  |
+|       bị tước đi       |
+0------------------------0
+''')
