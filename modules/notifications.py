c1b5f6de34ba6f7d162c38dd1a92ee88efcf1e91
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2019-11-19 13:09:41 +0700

    Change "Splash" to "Notifications"

---
+++ modules/notifications.py
@@ -0,0 +1,23 @@
+class Notifications():
+    '''
+    Notification prints for the application, tucked away for cleanliness
+    '''
+    def splash(self):
+        print(
+            '0------------------------0\n'
+            '|                        |\n'
+            '|   BKSchedule Rewrite   |\n'
+            '|   Copyright (C) 2019   |\n'
+            '|      GNU GPL v3.0      |\n'
+            '|                        |\n'
+            '|  Coder: PythonTryHard  |\n'
+            '|  Lang : Python 3       |\n'
+            '|  Ver  : v1.0           |\n'
+            '|                        |\n'
+            '|  Phần mềm open-source  |\n'
+            '|                        |\n'
+            '| Vì sự tiện lợi của bạn |\n'
+            '|  không phải là thứ để  |\n'
+            '|       bị tước đi       |\n'
+            '0------------------------0')
+        return
