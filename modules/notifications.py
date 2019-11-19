eb19ce9a65f1ec1ac5ab780fc2c0181e8d60e961
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2019-11-19 14:59:01 +0700

    Completed(?) notification module

--- modules/notifications.py
+++ modules/notifications.py
@@ -21,3 +21,36 @@ class Notifications():
             '|       bị tước đi       |\n'
             '0------------------------0')
         return
+
+    def password_input_note(self):
+        print(
+            '*----------------------------------------*\n'
+            '| Ghi chú: Mật khẩu được tự che khi nhập |\n'
+            '*----------------------------------------*'
+        )
+        return
+        
+    def first_run(self):
+        print(
+            '*------------------------------------------------*\n'
+            '|      Khai báo thông tin MyBK lần duy nhất      |\n'
+            '| Lưu ý: Nhập sai quá 3 lần tài khoản sẽ bị khóa |\n'
+            '*------------------------------------------------*')
+        self.password_input_note()
+        return
+
+    def second_pw_setup(self):
+        print(
+            '*-------------------------------------------------*\n'
+            '|       VUI LÒNG THIẾT LẬP MẬT KHẨU CẤP HAI       |\n'
+            '| ĐỂ BẢO VỆ THÔNG TIN TÀI KHOẢN MYBK LƯU TRÊN MÁY |\n'
+            '*-------------------------------------------------*')
+        return
+
+    def second_pw_note(self):
+        print(
+            '*---------------------------------------------------------------------*\n'
+            '| Ghi chú: Mật khẩu cấp 2 dùng để đăng nhập ứng dụng, không phải MyBK |\n'
+            '*---------------------------------------------------------------------*')
+        self.password_input_note()
+        return
